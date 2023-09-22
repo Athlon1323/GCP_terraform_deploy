36e6fa6509a9cc3655b1cd2daa242df34efebe61
Athlon1323 <ant.tkca44uk@gmail.com>
2023-09-22 14:03:53 +0400

    2

--- GCP_terraform+ansible/deply 3 instance and start playbook/1.tf
+++ GCP_terraform+ansible/deply 3 instance and start playbook/1.tf
@@ -40,7 +40,6 @@ resource "google_compute_instance" "web_servers" {
       image = local.image
     }
   }
-
   network_interface {
     network = local.network
     access_config {
@@ -51,6 +50,38 @@ resource "google_compute_instance" "web_servers" {
   }
 }
 
+resource "null_resource" "instance_cfg" {
+ count = length(google_compute_instance.web_servers)
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'",
+    "sudo apt update",
+    "sudo apt install ansible -y"
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = google_compute_instance.web_servers[count.index+1].network_interface[0].access_config[0].nat_ip
+    }
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "inventory.ini"
+  content = <<-EOT
+[web_servers]
+${join("\n", [for instance in google_compute_instance.web_servers : instance.network_interface[0].access_config[0].nat_ip])}
+  EOT
+}
+
+resource "null_resource" "run_ansible" {
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory.ini nginx_playbook.yml"
+  }
+}
 output "web_server_ips" {
   value = [for instance in google_compute_instance.web_servers : instance.network_interface[0].access_config[0].nat_ip]
 }
